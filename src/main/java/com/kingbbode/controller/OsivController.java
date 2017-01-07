88af53af19ca8d717c0312cd4977e9f9012e45fa
권용근 <kingbbode@gmail.com>
2017-01-07 20:16:39 +0900

    clean

--- src/main/java/com/kingbbode/controller/OsivController.java
+++ src/main/java/com/kingbbode/controller/OsivController.java
@@ -23,10 +23,4 @@ public class OsivController {
         model.addAttribute("members", memberRepository.findAll());
         return "index";
     }
-
-    @GetMapping("/proxy/test")
-    public String test(Model model) {
-        model.addAttribute("members", memberRepository.findAll());
-        return "index";
-    }
 }
